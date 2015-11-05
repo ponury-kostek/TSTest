f14ddcaa33da13d71ad32977d01be9e3aee286fa
Michał Żaloudik <ponury.kostek@gmail.com>
2015-11-05 14:54:38 +0100

    save

---
+++ index.ts
@@ -0,0 +1,11 @@
+"use strict";
+import M from './test';
+import N from './';
+class asd {
+   static f() {
+        return false;
+    }
+}
+M.f();
+N.f();
+export default asd;
---
+++ test.ts
@@ -0,0 +1,8 @@
+export default M;
+namespace M {
+    export var s = "hello";
+    export function f() {
+        return s;
+    }
+
+}
